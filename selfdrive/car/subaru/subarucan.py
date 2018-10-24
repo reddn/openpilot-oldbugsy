7aac57088978a3c84565411bce097b45c81b2c2d
Bugsy <30946888+bugsy924@users.noreply.github.com>
2018-10-24 23:56:47 +1100

    temporarily disabled xv and changed steer for outback/legacy

--- selfdrive/car/subaru/subarucan.py
+++ selfdrive/car/subaru/subarucan.py
@@ -1,16 +1,15 @@
 from selfdrive.car.subaru.values import CAR, DBC
 
-def create_steering_control(packer, bus, car_fingerprint, idx, steer1, byte2, checksum):
+def create_steering_control(packer, bus, car_fingerprint, idx, steer, checksum):
   if car_fingerprint in (CAR.OUTBACK, CAR.LEGACY):
 
     values = {
-      "Byte0": idx,
-      "Byte1": steer1,
-      "Byte2": byte2,
-      "Byte3": 1 if steer1 + byte2 != 0 else 0,
+      "Counter": idx,
+      "LKAS_Output": steer,
+      "LKAS_Active": 1 if steer != 0 else 0,
       "Checksum": checksum
     }
-    
+'''    
   if car_fingerprint == CAR.XV2018:
     values = {
       "Checksum": checksum,
@@ -18,5 +17,5 @@ def create_steering_control(packer, bus, car_fingerprint, idx, steer1, byte2, ch
       "Byte2": steer1,
       "Byte3": byte2
     }
-
+'''
   return packer.make_can_msg("ES_LKAS", 0, values)
